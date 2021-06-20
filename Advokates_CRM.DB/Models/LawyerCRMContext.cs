d4654c500bb26619611fc71ac43cbdae242b6fd7
Dmitry <xenich@gmail.com>
2021-06-20 16:21:36 +0300

    Добавление уникального индекса CaseId-CompanyUID в сущность Case

--- Advokates_CRM.DB/Models/LawyerCRMContext.cs
+++ Advokates_CRM.DB/Models/LawyerCRMContext.cs
@@ -33,10 +33,7 @@ namespace Advokates_CRM.DB.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-                /*
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-IDFEOG1;Database=LawyerCRM;Trusted_Connection=True;");
-                */
+
             }
         }
 
@@ -71,6 +68,10 @@ namespace Advokates_CRM.DB.Models
             modelBuilder.Entity<Case>(entity =>
             {
                 entity.HasKey(e => e.Uid);
+
+                entity.HasIndex(e => new { e.IdPerCompany, e.CompanyUid })
+                    .HasName("IX_Case_Company")
+                    .IsUnique();
 
                 entity.Property(e => e.Uid)
                     .HasColumnName("UID")
